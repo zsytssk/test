4ea6abc489147b2bd9156ba743a591549fa8144f
ZHANGSHIYANG341 <ZHANGSHIYANG341@pingan.com.cn>
2017-08-07 10:05:35 +0800

    master:learn 2017-08-07

--- angular-test/src/app/app.component.ts
+++ angular-test/src/app/app.component.ts
@@ -7,7 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hh';
+  selectedHero: Hero;
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado')
+  ];
   onClick() {
     console.log(1);
   }
+  getVal() {
+    return 10;
+  }
+  onSelect(hero: Hero) {
+    console.log(hero);
+    this.selectedHero = hero;
+  }
+}
+export class Hero {
+  constructor(public id: number,
+    public name: string) { }
 }
