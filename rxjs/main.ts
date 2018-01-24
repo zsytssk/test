0996ba27061253308a447381f80a2af8448ae274
张世阳 <zsytssk@gmail.com>
2018-01-24 21:58:09 +0800

    upload change

--- rxjs/main.ts
+++ rxjs/main.ts
@@ -1,20 +1,5 @@
 import { Observable } from 'rxjs/Rx';
 import { log } from './utils';
 
-const myObserverable = Observable.create(observer => {
-  observer.next('foo');
-  setTimeout(() => {
-    observer.next('bar');
-  }, 1000);
-});
-
-myObserverable
-  .filter(value => {
-    return value === 'bar';
-  })
-  .map(() => state => Object.assign({}, state, { count: state.count + 1 }))
-  .scan((state, changeFn) => changeFn(state), { count: 0 })
-  .subscribe(fn => {
-    log(fn);
-    // log(value);
-  });
+const source = Observable.timer(1000, 4000);
+const subscribe = source.subscribe(val => log(val));
