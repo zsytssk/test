add3df45ff08cb69c14a774f634a2f5ffccbf8e4
张世阳 <zsytssk@gmail.com>
2018-03-06 10:15:02 +0800

    master:[feature] course to 9

--- react_test/src/main.tsx
+++ react_test/src/main.tsx
@@ -3,10 +3,43 @@ import * as React from "react";
 import { render } from "react-dom";
 
 const rootElement = document.getElementById("root");
-
-function Message({ message }) {
-  return <div>{message ? message : "no message"}</div>;
+type State = {
+  lapse: number;
+  running: boolean;
+};
+class StopWatch extends React.Component<State, State> {
+  timer: number;
+  state = { lapse: 0, running: false };
+  handleRunClick = () => {
+    const startTime = Date.now() - this.state.lapse;
+    this.setState(state => {
+      if (!state.running) {
+        this.timer = setInterval(() => {
+          this.setState({ lapse: Date.now() - startTime });
+        }, 1);
+      } else {
+        clearInterval(this.timer);
+      }
+      return { running: !state.running };
+    });
+  };
+  handleClearClick = () => {
+    clearInterval(this.timer);
+    this.setState({ lapse: 0, running: false });
+  };
+  render() {
+    const { lapse, running } = this.state;
+    return (
+      <div>
+        <label>{`${lapse}ms`}</label>
+        <button onClick={this.handleRunClick}>
+          {running ? "stop" : "start"}
+        </button>
+        <button onClick={this.handleClearClick}>clear</button>
+      </div>
+    );
+  }
 }
+const element = <StopWatch lapse={0} running={false} />;
 
-const element = <Message message={null} />;
 render(element, rootElement);
