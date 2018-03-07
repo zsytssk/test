e36ac064aedbc56cc69cda365ba0ac1be8f5e93f
张世阳 <zsytssk@gmail.com>
2018-03-07 09:59:43 +0800

    master:upload change

--- react_test/src/main.tsx
+++ react_test/src/main.tsx
@@ -3,43 +3,32 @@ import * as React from "react";
 import { render } from "react-dom";
 
 const rootElement = document.getElementById("root");
-type State = {
-  lapse: number;
-  running: boolean;
-};
-class StopWatch extends React.Component<State, State> {
-  timer: number;
-  state = { lapse: 0, running: false };
-  handleRunClick = () => {
-    const startTime = Date.now() - this.state.lapse;
-    this.setState(state => {
-      if (!state.running) {
-        this.timer = setInterval(() => {
-          this.setState({ lapse: Date.now() - startTime });
-        }, 1);
-      } else {
-        clearInterval(this.timer);
-      }
-      return { running: !state.running };
-    });
+
+class NameForm extends React.Component<any, any> {
+  componentDidMount() {
+    console.log(this);
+  }
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.inputNode.value);
   };
-  handleClearClick = () => {
-    clearInterval(this.timer);
-    this.setState({ lapse: 0, running: false });
-  };
+  inputNode: HTMLInputElement;
   render() {
-    const { lapse, running } = this.state;
     return (
-      <div>
-        <label>{`${lapse}ms`}</label>
-        <button onClick={this.handleRunClick}>
-          {running ? "stop" : "start"}
-        </button>
-        <button onClick={this.handleClearClick}>clear</button>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="">
+          name:
+          <input
+            type="text"
+            name="username"
+            ref={node => (this.inputNode = node)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     );
   }
 }
-const element = <StopWatch lapse={0} running={false} />;
 
+const element = <NameForm />;
 render(element, rootElement);
