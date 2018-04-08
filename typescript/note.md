ff439816885aa4f9ae56333909c99c9085b55014
张世阳 <zsytssk@gmail.com>
2018-04-08 18:08:36 +0800

    master:[sync]

--- typescript/note.md
+++ typescript/note.md
@@ -1,5 +1,9 @@
 ## 2018-04-08 16:14:03
 
+* @ques 如何只提取public属性
+* @ques 如何只提取private属性
+* @ques ...
+
 ```ts
 type BoxedValue<T> = { value: T };
 type BoxedArray<T> = { value: T[] };
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,14 +1,4 @@
-var C = function (p) {
-    this.p = p;
-};
-C.prototype = {
-    m() {
-        console.log(this.p);
-    }
-};
-C.prototype.q = function (r) {
-    return this.p === r;
-};
-
-let a = new C(1);
-a.q();
+type Props<T> = {
+    -readonly [k in keyof T]: T[k]
+}
+type T = Props<Laya.Node>;
