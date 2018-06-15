1fde1a0488ce464708b5b06fa9439ca7e54ffe20
zsytssk <zsytssk@gmail.com>
2018-06-15 09:59:05 +0800

    [upload]

--- jsx/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const createElement_1 = require("./jsx/createElement");
-let a = (createElement_1.createElement("div", { className: "test" },
-    createElement_1.createElement("span", null, "sdfsdfsdfsf")));
--- jsx/webpack.config.dev.js
+++ jsx/webpack.config.dev.js
@@ -1,28 +1,28 @@
-"use strict";
-const path = require("path");
+'use strict';
+const path = require('path');
 
 module.exports = {
-  devtool: "cheap-module-eval-source-map",
-  entry: ["./src/main.ts"],
+  devtool: 'cheap-module-eval-source-map',
+  entry: ['./src/main.ts'],
   output: {
-    filename: "main.js",
-    path: path.join(__dirname, "dist")
+    filename: 'main.js',
+    path: path.join(__dirname, 'dist'),
   },
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.(.*)?$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         options: {
-          configFile: "tsconfig.webpack.json",
-          transpileOnly: true
-        }
-      }
-    ]
+          configFile: 'tsconfig.webpack.json',
+          transpileOnly: true,
+        },
+      },
+    ],
   },
   watch: true,
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  }
+    extensions: ['.ts', '.tsx', '.js'],
+  },
 };
