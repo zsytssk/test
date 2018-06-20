e2aba9d74185b0b9a67c210c6b856706c58d83bd
zsytssk <zsytssk@gmail.com>
2018-06-20 10:08:17 +0800

    [update] virtual dom

--- jsx/src/jsx/createElement.ts
+++ jsx/src/jsx/createElement.ts
@@ -1,8 +1,129 @@
-export function createElement(node) {
+export function h(type, props, ...children) {
+  return { type, props, children };
+}
+
+function createElement(node) {
   if (typeof node === 'string') {
     return document.createTextNode(node);
   }
   const $el = document.createElement(node.type);
+  setProps($el, node.props);
   node.children.map(createElement).forEach($el.appendChild.bind($el));
   return $el;
+}
+
+function changed(node1, node2) {
+  return (
+    typeof node1 !== typeof node2 ||
+    (typeof node1 === 'string' && node1 !== node2) ||
+    node1.type !== node2.type ||
+    (node1.props && node1.props.forceUpdate)
+  );
+}
+
+export function updateElement($parent, newNode, oldNode?, index = 0) {
+  if (!oldNode) {
+    $parent.appendChild(createElement(newNode));
+  } else if (!newNode) {
+    $parent.removeChild($parent.childNodes[index]);
+  } else if (changed(newNode, oldNode)) {
+    $parent.replaceChild(createElement(newNode), $parent.childNodes[index]);
+  } else if (newNode.type) {
+    updateProps($parent.childNodes[index], newNode.props, oldNode.props);
+
+    const newLength = newNode.children.length;
+    const oldLength = oldNode.children.length;
+    for (let i = 0; i < newLength || i < oldLength; i++) {
+      updateElement(
+        $parent.childNodes[index],
+        newNode.children[i],
+        oldNode.children[i],
+        i,
+      );
+    }
+  }
+}
+
+function setProps($target, props) {
+  if (!props) {
+    return;
+  }
+  Object.keys(props).forEach(name => {
+    setProp($target, name, props[name]);
+  });
+}
+function setProp($target, name, value) {
+  if (isCustomProp(name)) {
+    return;
+  } else if (isEventProp(name)) {
+    addEventListener($target, name, value);
+  } else if (name === 'className') {
+    $target.setAttribute('class', value);
+  } else if (typeof value === 'boolean') {
+    setBooleanProp($target, name, value);
+  } else {
+    $target.setAttribute(name, value);
+  }
+}
+
+function setBooleanProp($target, name, value) {
+  if (value) {
+    $target.setAttribute(name, value);
+    $target[name] = true;
+  } else {
+    $target[name] = false;
+  }
+}
+
+function isCustomProp(name) {
+  return name === 'forceUpdate';
+}
+
+function updateProps($target, newProps, oldProps = {}) {
+  const props = Object.assign({}, newProps, oldProps);
+  Object.keys(props).forEach(name => {
+    updateProp($target, name, newProps && newProps[name], oldProps[name]);
+  });
+}
+
+function updateProp($target, name, newVal, oldVal) {
+  if (!newVal) {
+    removeProp($target, name, oldVal);
+  } else if (!oldVal || newVal !== oldVal) {
+    setProp($target, name, newVal);
+  }
+}
+
+function removeBooleanProp($target, name) {
+  $target.removeAttribute(name);
+  $target[name] = false;
+}
+
+function removeProp($target, name, value) {
+  if (isCustomProp(name)) {
+    return;
+  } else if (isEventProp(name)) {
+    removeEventListener($target, name, value);
+  } else if (name === 'className') {
+    $target.removeAttribute('class');
+  } else if (typeof value === 'boolean') {
+    removeBooleanProp($target, name);
+  } else {
+    $target.removeAttribute(name);
+  }
+}
+
+function isEventProp(name) {
+  return /^on/.test(name);
+}
+
+function extractEventName(name) {
+  return name.slice(2).toLowerCase();
+}
+
+function addEventListener($target, name, prop) {
+  $target.addEventListener(extractEventName(name), prop);
+}
+function removeEventListener($target, name, prop) {
+  $target.removeEventListener(extractEventName(name), prop);
 }
--- jsx/src/main.tsx
+++ jsx/src/main.tsx
@@ -1,8 +1,38 @@
-import { createElement } from './jsx/createElement';
-let a = (
-  <div className="test">
-    <span>sdfsdfsdfsf</span>
-  </div>
+import { h, updateElement } from './jsx/createElement';
+function log(e) {
+  console.log(e.target.value);
+}
+
+const f = (
+  <ul style="list-style: none;">
+    <li className="item" onClick={() => alert('hi!')}>
+      item 1
+    </li>
+    <li className="item">
+      <input type="checkbox" checked={true} />
+      <input type="text" onInput={log} />
+    </li>
+    {/* this node will always be updated */}
+    <li forceUpdate={true}>text</li>
+  </ul>
 );
 
-console.log(a);
+const g = (
+  <ul style="list-style: none;">
+    <li className="item">item 1</li>
+    <li className="item">
+      <input type="checkbox" checked={true} />
+      <input type="text" />
+    </li>
+    {/* this node will always be updated */}
+    <li forceUpdate={true}>text</li>
+  </ul>
+);
+
+const $root = document.getElementById('root');
+const $reload = document.getElementById('reload');
+
+updateElement($root, f);
+$reload.addEventListener('click', () => {
+  updateElement($root, g, f);
+});
