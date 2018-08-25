d40a73754937443286ee36a3c55ca6b6c1da432c
zsytssk <zsytssk@gmail.com>
2018-08-25 16:34:18 +0800

    [update]

--- typescript/note.md
+++ typescript/note.md
@@ -1,16 +1,24 @@
 ## 2018-04-18 08:49:25
-* @ques 能不能将object属性中 包含_去掉
 
-* @ques 能不能让 extends<T, U> U存在用U类型, 不然用T类型
+- @note 新建绑定 + 销毁取消...
 
-* @ques  Destructuring with rest 类型如何处理
-`{const children, let ...other} = this.props as Panel[];`
+- @ques 能不能将 model+socket 的依赖独立出来
+
+  - 同时相互创建 下级元素 依赖 如何取消
+  - 而且还有父类子类不同的绑定如何处理...
+
+- @ques 能不能将 object 属性中 包含\_去掉
+
+- @ques 能不能让 extends<T, U> U 存在用 U 类型, 不然用 T 类型
+
+- @ques Destructuring with rest 类型如何处理
+  `{const children, let ...other} = this.props as Panel[];`
 
 ## 2018-04-08 16:14:03
 
-* @ques 如何只提取public属性
-* @ques 如何只提取private属性
-* @ques ...
+- @ques 如何只提取 public 属性
+- @ques 如何只提取 private 属性
+- @ques ...
 
 ```ts
 type BoxedValue<T> = { value: T };
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,18 +1,49 @@
-function testable(Class): typeof Class {
-  return (...args) => {
-    console.log(args);
-    return new Class(...args);
+import { BaseEvent } from "./event";
+
+function inject(comp) {
+  return function(Class): typeof Class {
+    return (...args) => {
+      const a = new Class(...args);
+      comp(a);
+      return a;
+    };
   };
 }
 
-@testable
-class MyTestableClass {
+class ClassB {
+  target: BaseEvent;
+  constructor(target: BaseEvent) {
+    this.target = target;
+
+    this.init();
+  }
+  private init() {
+    const { target } = this;
+    target.on("destroy", () => {
+      this.unBindCtrl();
+    });
+
+    this.bindCtrl();
+  }
+  private bindCtrl() {
+    const { target } = this;
+    target.on("destroy", () => {
+      this.unBindCtrl();
+    });
+  }
+  private unBindCtrl() {}
+}
+
+@inject(ClassB)
+class CtrlA extends BaseEvent {
   dd = 1;
+  components = [];
   constructor(...args) {
+    super();
     console.log(args);
   }
   // ...
 }
 
-const a = new MyTestableClass(1, 2, 3);
-console.log(a.dd);
+const a = new CtrlA(1, 2, 3);
+console.log(a);
