5c054dc7de7ff218534bcab7a9aee159ddcc18a8
张世阳 <ZHANGSHIYANG162@pingan.com.cn>
2019-04-26 10:25:32 +0800

    feature/test_render_data:[save]

--- rust/normal/src/main.rs
+++ rust/normal/src/main.rs
@@ -1,7 +1,29 @@
-extern crate lib;
-use lib::get_img_buffer;
+#[macro_use]
+extern crate futures;
+extern crate tokio;
+
+mod future;
+mod interval;
+
+use self::future::IntervalFuture;
+use self::interval::Interval;
+use tokio::prelude::*;
+
+struct IntervalPrinter(IntervalFuture);
+
+impl Future for IntervalPrinter {
+    type Item = ();
+    type Error = ();
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        let curr = try_ready!(self.0.poll());
+        println!("Counter is: {}", curr);
+        Ok(Async::Ready(()))
+    }
+}
 
 fn main() {
-    let data = get_img_buffer("test.jpg");
-    data.save("test1.jpg").unwrap();
+    let interval = Interval::from_millis(500);
+    let interval_future = IntervalFuture::new(interval);
+    let interval_printer = IntervalPrinter(interval_future);
+    tokio::run(interval_printer)
 }
