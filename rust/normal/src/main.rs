136f7ab3db2fe137793d761ef7f9c1a1c2dc8d61
张世阳 <ZHANGSHIYANG162@pingan.com.cn>
2019-05-06 10:08:16 +0800

    feature/test_render_data:[update]

--- rust/normal/src/main.rs
+++ rust/normal/src/main.rs
@@ -1,43 +1,57 @@
-extern crate futures;
 extern crate tokio;
 
-mod interval;
-mod stream;
-
-use self::interval::Interval;
-use self::stream::IntervalStream;
+use std::fs::File;
+use std::net::SocketAddr;
+use std::net::ToSocketAddrs;
+use tokio::io::{read_to_end, write_all};
+use tokio::net::TcpStream;
 use tokio::prelude::*;
 
-struct MyOk<T>(Option<T>, u32);
-impl<T> MyOk<T> {
-    fn new(t: T) -> MyOk<T> {
-        MyOk(Some(t), 0)
-    }
+fn download(host: String, path: String, filename: String) -> impl Future<Item = (), Error = ()> {
+    let mut addr_iter = host.to_socket_addrs().unwrap();
+    let addr = match addr_iter.next() {
+        None => panic!("DNS resolution failed"),
+        Some(addr) => addr,
+    };
+    let req_body = format!(
+        "GET {} HTTP/1.1\r\nHost: {}:80\r\nConnection: close\r\n\r\n",
+        path, host,
+    );
+
+    TcpStream::connect(&addr)
+        .and_then(|stream| {
+            write_all(stream, req_body).and_then(|(stream, _body)| {
+                let buffer = vec![];
+                read_to_end(stream, buffer).and_then(|(_stream, buffer)| {
+                    File::create(filename).and_then(|mut file| file.write_all(&buffer))
+                })
+            })
+        })
+        .map_err(|e| eprintln!("Error occured: {:?}", e))
 }
 
-impl<T> Future for MyOk<T> {
-    type Item = T;
-    type Error = ();
-    fn poll(&mut self) -> Poll<T, ()> {
-        let state = self.1;
-        println!("MyOk state  = {}", self.1);
-        if state == 0 {
-            self.1 = 1;
-            let task = futures::task::current();
-            task.clone().notify();
-            Ok(Async::NotReady)
-        } else {
-            Ok(Async::Ready(self.0.take().unwrap()))
-        }
+fn resolve_addr(host: &str) -> Result<SocketAddr, String> {
+    let mut addr_iter = match host.to_socket_addrs() {
+        Ok(addr_iter) => addr_iter,
+        Err(e) => return Err(format!("Invalid host name {:?}: {:?}", host, e)),
+    };
+
+    match addr_iter.next() {
+        None => Err(format!("No addresses found for host: {:?}", host)),
+        Some(addr) => Ok(addr),
     }
 }
 
 fn main() {
-    let name = String::from("Alice");
-    let future = MyOk::new(name).and_then(|name| {
-        println!("Name: {}", name);
-        MyOk::new(())
-    });
-    println!("test");
-    tokio::run(future);
+    tokio::run(future::poll_fn(|| {
+        let mut args = std::env::args().skip(1);
+        loop {
+            match (args.next(), args.next(), args.next()) {
+                (Some(host), Some(path), Some(filename)) => {
+                    tokio::spawn(download(host, path, filename));
+                }
+                _ => return Ok(Async::Ready(())),
+            }
+        }
+    }))
 }
