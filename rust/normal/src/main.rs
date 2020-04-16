9676c8b66fc92896b69c7e426a242716ccb5d316
zsytssk <zsytssk@gmail.com>
2020-04-16 09:39:08 +0800

    master:

--- rust/normal/src/main.rs
+++ rust/normal/src/main.rs
@@ -1,54 +1,33 @@
-#![allow(unused)]
+/// A filter over an iterator
+struct Filter<I, F> {
+    iter: I,
+    pred: F,
+}
 
-use std::marker::PhantomPinned;
-use std::pin::Pin;
-use std::ptr::NonNull;
+/// Constructs a filter
+fn filter<I, F>(iter: I, pred: F) -> Filter<I, F> {
+    Filter { iter: iter, pred: pred }
+}
+
+impl<I, F> Iterator for Filter<I, F>
+    where I: Iterator,
+          F: Fn(&I::Item) -> bool,  // Magic! What's the lifetime?
+{
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<I::Item> {
+        while let Some(val) = self.iter.next() {
+            if (self.pred)(&val) {
+                return Some(val);
+            }
+        }
+        None
+    }
+}
 
 fn main() {
-    // This is a self-referential struct because the slice field points to the data field.
-    // We cannot inform the compiler about that with a normal reference,
-    // as this pattern cannot be described with the usual borrowing rules.
-    // Instead we use a raw pointer, though one which is known not to be null,
-    // as we know it's pointing at the string.
-    struct Unmovable {
-        data: String,
-        slice: NonNull<String>,
-        _pin: PhantomPinned,
+    let x = vec![1, 2, 3, 4, 5];
+    for v in filter(x.into_iter(), |v: &i32| *v % 2 == 0) {
+        println!("{}", v); // 2, 4
     }
-
-    impl Unmovable {
-        // To ensure the data doesn't move when the function returns,
-        // we place it in the heap where it will stay for the lifetime of the object,
-        // and the only way to access it would be through a pointer to it.
-        fn new(data: String) -> Pin<Box<Self>> {
-            let res = Unmovable {
-                data,
-                // we only create the pointer once the data is in place
-                // otherwise it will have already moved before we even started
-                slice: NonNull::dangling(),
-                _pin: PhantomPinned,
-            };
-            let mut boxed = Box::pin(res);
-
-            let slice = NonNull::from(&boxed.data);
-            // we know this is safe because modifying a field doesn't move the whole struct
-            unsafe {
-                let mut_ref: Pin<&mut Self> = Pin::as_mut(&mut boxed);
-                Pin::get_unchecked_mut(mut_ref).slice = slice;
-            }
-            boxed
-        }
-    }
-
-    let unmoved = Unmovable::new("hello".to_string());
-    // The pointer should point to the correct location,
-    // so long as the struct hasn't moved.
-    // Meanwhile, we are free to move the pointer around.
-    #[allow(unused_mut)]
-    let mut still_unmoved = unmoved;
-    assert_eq!(still_unmoved.slice, NonNull::from(&still_unmoved.data));
-
-    // Since our type doesn't implement Unpin, this will fail to compile:
-    // let mut new_unmoved = Unmovable::new("world".to_string());
-    // std::mem::swap(&mut *still_unmoved, &mut *new_unmoved);
 }
