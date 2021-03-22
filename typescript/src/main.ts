8b2d9fc6c44e7a8ba6848e9da7c6a74e10a1e32b
zsytssk <zsytssk@gmail.com>
2021-03-22 09:58:29 +0800

    master:

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -8,25 +8,8 @@ async function main() {
     const source = await readFile(file_path);
     const ast = recast.parse(source, { parser: tsParser });
     const item = ast.program.body[0].declaration;
-    var a = eval(`JSON.stringify(${recast.print(item).code})`);
-    const oriObj = JSON.parse(a);
-    // console.log(JSON.parse(recast.print(item).code));
-    // console.log(recast.print(item).code);
-    const result = findKeys(oriObj);
-    console.log(result);
+    var a = eval(`var a = ${recast.print(item).code}; a`);
+    console.log(a);
 }
 
 main();
-
-function findKeys(oriObj: any, prefix = '', newObj = {}) {
-    for (const key of Object.keys(oriObj)) {
-        const value = oriObj[key];
-        if (typeof value === 'string') {
-            newObj[prefix + key] = value;
-        } else {
-            findKeys(value, `${prefix}${key}_`, newObj);
-        }
-    }
-
-    return newObj;
-}
